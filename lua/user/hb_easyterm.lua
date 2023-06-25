d7ead78b656c3a0587be67af954413e75fbe47e3
Henrique Braga <henriquebraga@Henriques-MacBook-Air.local>
2023-06-25 18:55:29 +0100

    few new functionalities to hbeasy

--- lua/user/hb_easyterm.lua
+++ lua/user/hb_easyterm.lua
@@ -1,9 +1,11 @@
+local async = require "plenary.async"
+
 _Hbeasy = {}
 
 function HbEasyTermStart()
     local width = vim.api.nvim_win_get_width(0)
     vim.cmd("tabnew")
-    vim.cmd("e /Users/henriquebraga/development/hb_easyterm_commands.txt")
+    vim.cmd("e ~/development/hb_easyterm_commands.txt")
     _Hbeasy.command_window = vim.api.nvim_get_current_win()
     vim.cmd("vsplit")
     vim.cmd("vertical resize +" .. (width/2)*.5)
@@ -26,16 +28,66 @@ function HbEasyTermSend()
         return
     end
     local currentReg = getRegister('"')
+
     vim.cmd("norm vipy")
     vim.api.nvim_set_current_win(_Hbeasy.term_window)
     vim.cmd("norm pi")
     local returnKeycode = vim.api.nvim_replace_termcodes('<Cr><Esc><C-w>h', true, false, true)
     vim.api.nvim_feedkeys(returnKeycode, 't', false)
+
     setRegister('"', currentReg)
+end
+
+function HbEasyTermSendLine()
+    local buffer_name = vim.api.nvim_buf_get_name(0)
+    if not string.find(buffer_name, 'hb_easyterm_commands') then
+        return
+    end
+    local currentReg = getRegister('"')
+
+    vim.cmd("norm yy")
+    vim.api.nvim_set_current_win(_Hbeasy.term_window)
+    vim.cmd("norm pi")
+    local returnKeycode = vim.api.nvim_replace_termcodes('<Cr><Esc><C-w>h', true, false, true)
+    vim.api.nvim_feedkeys(returnKeycode, 't', false)
+
+    setRegister('"', currentReg)
+end
+
+function HbEasyTermTab()
+    local buffer_name = vim.api.nvim_buf_get_name(0)
+    if not string.find(buffer_name, 'hb_easyterm_commands') then
+        return
+    end
+
+    vim.cmd("norm yy")
+    local command = getRegister('"')
+    vim.api.nvim_set_current_win(_Hbeasy.term_window)
+    vim.cmd("norm i")
+    local cleanline_code = vim.api.nvim_replace_termcodes('<C-u>', true, false, true)
+    vim.api.nvim_feedkeys(cleanline_code, 't', false)
+    vim.api.nvim_feedkeys(command, 't', false)
+    local tab_keycode = vim.api.nvim_replace_termcodes('<BS><BS><Tab><Esc><C-w>h', true, false, true)
+    vim.api.nvim_feedkeys(tab_keycode, 't', false)
+end
+
+function HbEasyTermGetCommand()
+    local buffer_name = vim.api.nvim_buf_get_name(0)
+    if not string.find(buffer_name, 'hb_easyterm_commands') then
+        return
+    end
+
+    vim.api.nvim_set_current_win(_Hbeasy.term_window)
+    local get_command = vim.api.nvim_replace_termcodes('i<Esc>vBy<C-w>h$vBlplx', true, false, true)
+    vim.api.nvim_feedkeys(get_command, 't', false)
 end
 
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>wt", ":lua HbEasyTermStart()<Cr>", opts)
-keymap("n", "<leader>t", ":lua HbEasyTermSend()<Cr>", opts)
+keymap("n", "<leader>c", ":lua HbEasyTermSend()<Cr>", opts)
+keymap("n", "<leader>l", ":lua HbEasyTermSendLine()<Cr>", opts)
+keymap("n", "<leader>t", ":lua HbEasyTermTab()<Cr>", opts)
+keymap("n", "<leader>g", ":lua HbEasyTermGetCommand()<Cr>", opts)
+
